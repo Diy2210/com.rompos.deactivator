47842e64750f4864f9456c9386ad3fd92bb8ecdc
Dmitriy <diy2210@gmail.com>
2020-08-07 17:15:53 +0300

    rename;

--- mpp-library/src/commonMain/kotlin/com/rompos/deactivator/ServerRepository.kt
+++ mpp-library/src/commonMain/kotlin/com/rompos/deactivator/ServerRepository.kt
@@ -6,27 +6,27 @@ open class ServerRepository {
     private val database = createDB()
     private val serverQueries = database.serverQueries
 
-    // Insert New Plugins
+    // Insert New Servers
     fun insert(server: Servers) {
         serverQueries.insertServer(server.title, server.url, server.token)
     }
 
-    // Get All Plugins
+    // Get All Servers
     fun list(): List<Servers> {
         return serverQueries.selectAll().executeAsList()
     }
 
-    // Get Plugin by ID
+    // Get Server by ID
 //    fun get(id: Long): List<Plugins>? {
 //        return pluginQueries.selectPlugin(id)
 //    }
 
-    // Update Plugin
+    // Update Server
     fun update(id: Long, title: String, url: String, token: String) {
         serverQueries.updateServer(title, url, token, id)
     }
 
-    // Delete Plugin
+    // Delete Server
     fun delete(id: Long) {
         serverQueries.deleteServer(id)
     }
